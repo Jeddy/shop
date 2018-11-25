03f5d7b19e4fefbf97fb68cac155c0f11869c6a1
Jeddy <jeddyliu@foxmail.com>
2018-11-25 14:39:17 +0000

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="#" target="_blank">Jeddy</a> 设计和编码</span>
+        </p>
+    </div>
+</footer>
